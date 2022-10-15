51dff9deec9b6e1c08fae95b091d91e5ae48a161
WHOISGLM <gilbertlouiemercene@gmail.com>
2022-10-15 23:48:40 +0800

    Start

---
+++ template-1/Anfra_v1.3/register-4/js/custom.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+	'use strict';
+
+  /*--------------------------------------------
+  Owl Carousel
+  --------------------------------------------*/
+
+  $('.ugf-slider').owlCarousel({
+    loop:true,
+    items: 1,
+    autoplay: true,
+    autoplayHoverPause: true,
+    dots: true,
+    nav: false,
+    autoplaySpeed: 600,
+    dotsSpeed: 600,
+    margin: 15
+  })
+
+})
